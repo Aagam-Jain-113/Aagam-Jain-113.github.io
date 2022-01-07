492f1d7fcbe626e87f094077e7a85eb3f11f10d8
Aagam-Jain-113 <aagamjain113@gmail.com>
2022-01-07 13:08:59 +0530

    added: Calendly button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs'
+import { BsLinkedin, BsTwitter } from 'react-icons/bs'
 import { GoMail } from 'react-icons/go'
-// import { PopupButton } from 'react-calendly'
+import { PopupButton } from 'react-calendly'
 
 function Contact() {
     return (
         <div className='max-w-screen-lg px-10 lg:px-0 mx-auto mt-12 lg:mt-24' id="contact">
             <hr className="h-0.5 bg-gray-300" />
             <div className='text-center text-2xl my-6'>
-                <p>You can connect with me on any of the following platforms</p>
+                <PopupButton
+                    url="https://calendly.com/aagamjain113?primary_color=36e7f7"
+                    className="w-full p-2 md:w-5/12 text-xl mt-6 text-white rounded-full bg-primary hover:bg-gray-700"
+                    text="Lets meet over a cup of coffee ☕!"
+                />
+                <p className='my-3 text-base'>or</p>
+                <p>You can connect with me on:</p>
                 <div className='flex space-x-8 justify-center text-2xl mt-4'>
-                    <a href="https://github.com/Aagam-Jain-113/" aria-label="GitHub" target="_blank" className="transform hover:scale-125 duration-300" rel="noreferrer">
-                        <BsGithub />
-                    </a>
                     <a href="https://www.linkedin.com/in/aagam-jain113/" aria-label="Linkedin" target="_blank" className="transform hover:scale-125 duration-300" rel="noreferrer">
                         <BsLinkedin />
                     </a>
@@ -23,14 +26,7 @@ function Contact() {
                         <BsTwitter />
                     </a>
                 </div>
-                <p className='text-gray-400 my-10 text-sm'>&#169; Aagam Jain 2022 </p>
-                {/* <p className='mt-4 text-base '>or</p>
-                <p className='mt-2'>We can have a one-on-one meeting over a cup of coffee</p>
-                <PopupButton
-                    url="https://calendly.com/aagamjain113?primary_color=36e7f7"
-                    className="w-1/5 px-2 text-xl py-2 mt-6 text-white rounded-full bg-primary hover:bg-darker"
-                    text="Lets connect!"
-                /> */}
+                <p className='text-gray-400 my-6 text-sm'>&#169; Aagam Jain 2022 </p>
             </div>
         </div>
     )
